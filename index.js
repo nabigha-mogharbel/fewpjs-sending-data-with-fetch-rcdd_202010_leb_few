7ac7d6f30f670945b539830ed3cf16ca5be2a972
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-29 01:33:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+
+function submitData(name,email){
+  return fetch('http://localhost:3000/users',{method: "POST",
+  headers: {'content-type': 'application/json' , 'Accept': 'application/json'},
+  body: JSON.stringify({name,email})})
+  .then(function(response) {
+    return response.json();
+  })
+  
+  .then(function(object) {
+    document.body.innerHTML=object['id'];
+         })
+  .catch(function(error) {
+    alert("Bad Thinghs! Ragnarők!")
+    document.body.innerHTML=error.message });
+}
+  
