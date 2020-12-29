961c8f41c7485f6bdc9ce9889bff667372dbd444
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-29 01:17:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ let configuration={method: "POST",
   body: JSON.stringify(name,email)};
 
 function submitData(name,email){
-  return fetch('http://localhost:3000/users',configuration)
+  return fetch('http://localhost:3000/users',{method: "POST",
+  headers: {'content-type': '/json' , 'Accept': 'application/json'},
+  body: JSON.stringify(name,email)})
   .then(function(response) {
     return response.json();
   })
-  .then(function(object) {document.getElementByTagName('body').innerHTML=`<p>${object}</p>`;
-    console.log(object);
-  })
+  
+  .then(function(response){return response.json})
   .catch(function(error) {
     alert('Unauthorized Access');
   });
