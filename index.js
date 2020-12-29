19fbf32cc204d13414af5113a52337171902782a
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-29 01:18:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // Add your code here
 
-let configuration={method: "POST",
-  headers: {'content-type': '/json' , 'Accept': 'application/json'},
-  body: JSON.stringify(name,email)};
-
 function submitData(name,email){
   return fetch('http://localhost:3000/users',{method: "POST",
   headers: {'content-type': 'application/json' , 'Accept': 'application/json'},
